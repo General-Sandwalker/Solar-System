c4c3b567eb4c541bbbd19e3618545847ff1e103e
Jaouadi Nada <jaouadinada19@gmail.com>
2024-12-01 18:16:37 +0100

    creation d'une animation en js

---
+++ animation.js
@@ -0,0 +1,33 @@
+function animéeSun(){
+    alert('Sun');
+}
+function animéeMercury(){
+    alert('Mercury');
+}
+function animéeVenus(){
+    alert('Venus');
+}
+function animéeEarth(){
+    alert('Earth');
+}
+function animéeMars(){
+    alert('Mars');
+}
+function animéeJupiter(){
+    alert('Jupiter');
+}
+function animéeSaturn(){
+    alert('Saturn');
+}
+function animéeJupiter(){
+    alert('Jupiter');
+}
+function animéeUranus(){
+    alert('Uranus');
+}
+function animéeNeptune(){
+    alert('Neptune');
+}
+function animéePluto(){
+    alert('Pluto');
+}
